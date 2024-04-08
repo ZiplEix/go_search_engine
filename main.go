cb8caa28cfb3b6558fc9e12ad25970a43650caf9
Baptiste Leroyer <86067803+ZiplEix@users.noreply.github.com>
2024-04-08 18:37:55 +0200

    first commit

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/compress"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic("cannot find environment variables")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = ":8080"
+	} else {
+		port = ":" + port
+	}
+
+	app := fiber.New(fiber.Config{
+		IdleTimeout: 5 * time.Second,
+	})
+
+	app.Use(compress.New())
+
+	// Start the server and listen for a shutdown
+	go func() {
+		if err := app.Listen(port); err != nil {
+			log.Panic(err)
+		}
+	}()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
+	<-c // Block the main thread until a signal is received
+
+	_ = app.Shutdown()
+	fmt.Println("Shutting down server")
+}
---
+++ readme.md
@@ -0,0 +1,22 @@
+# Go Search Engine
+
+## Features
+
+- [ ] Web crawler
+- [ ] Custom Full Text Search Engine
+- [ ] Cronjobs
+- [ ] Logged in section to control the crawler
+- [ ] JSON API
+- [ ] Rate limiting
+- [ ] Docker ??
+
+## Tech Stack
+
+- GO
+  - Fiber (not needed but i'm confortable with it)
+  - GORM
+  - Go HTML ot Templ
+- Postgres
+- Custom JWT Auth
+- Docker
+- Tailwind & daisyUI 😒
